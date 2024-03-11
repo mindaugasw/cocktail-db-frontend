f83a6aef8444dbae99fd9abed654ed66385bb4dc
Mindaugas <mindaugas932@gmail.com>
2024-03-11 23:51:00 +0200

    Install Bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
-import './assets/main.css'
+// import './assets/main.css'
 
-import { createApp } from 'vue'
-import App from './AppExample.vue'
-import router from './router'
+import { createApp } from 'vue';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import App from './AppExample.vue'
+import App from './App.vue';
+import router from './router';
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(router)
+app.use(router);
 
-app.mount('#app')
+app.mount('#app');
