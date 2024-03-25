85aa99c650bc487d5dc83ca8a1cd9b58af17c249
Mindaugas <mindaugas932@gmail.com>
2024-03-25 23:50:25 +0200

    Remove example files

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,8 @@
 import './assets/main.css'
-// import './assets/mainExample.css';
-
 import { createApp } from 'vue';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import FontAwesomeIcon from '@/components/FontAwesomeIcon';
-// import App from './AppExample.vue';
 import App from './App.vue';
 import router from '@/router';
 
